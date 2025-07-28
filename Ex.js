1a5773669df32e2ffb22f7147b83f99dff5fcc85
VGL <vitoriaglordeiro@gmail.com>
2025-07-28 14:56:38 -0300

    primeiro commit
    adequação do código ao que foi solicitado

---
+++ Ex.js
@@ -0,0 +1,9 @@
+function multiplicar(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+// Exemplos de uso:
+console.log(multiplicar(2, 3)); // 6
+console.log(saudacao("Vitória")); // Olá Vitória
---
+++ Ex.ts
@@ -0,0 +1,11 @@
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+// Exemplos de uso:
+console.log(multiplicar(2, 3)); // 6
+console.log(saudacao("Vitória")); // Olá Vitória
